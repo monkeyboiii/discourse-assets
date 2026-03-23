6662db7a3c19ce1b57fe04e687650f42542a6bb5
haolu_mstr <haolu@microstrategy.com>
2026-03-23 15:52:24 +0800

    rsync

--- emoji.sh
+++ emoji.sh
@@ -37,7 +37,7 @@ uv run \
 # Step 2: Copy xcassets into the Xcode project
 # ---------------------------------------------------------------------------
 echo "=== Step 2: Copy xcassets to App ==="
-cp -r assets/emojis/DiscourseEmojis.xcassets/ "$KIT_DIR/Resources"
+rsync -aEc assets/emojis/DiscourseEmojis.xcassets/ "$KIT_DIR/Resources"
 
 
 # ---------------------------------------------------------------------------
